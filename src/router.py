d8b843944f595905d5a542311a6ed76e9a7a7e97
Guilherme Torres <guilhermetorres97@gmail.com>
2018-10-20 20:18:57 -0300

    socket e tabela de routing OK

--- src/router.py
+++ src/router.py
@@ -1,9 +1,18 @@
 import argparse
+import socket
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("addr", help="endereco ao qual o roteador deve se associar", type=str)
     parser.add_argument("period", help="periodo entre envio de mensagens de update", type=float)
+    parser.add_argument("startup", nargs="?", help="arquivo com instrucoes que serao executadas no inicio", type=str)
     args = parser.parse_args()
+
+    # bind socket
+    sckt = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sckt.bind((args.addr, 55151))
+
+    # tabela de roteamento
+    routing_table = []
 
 main()
