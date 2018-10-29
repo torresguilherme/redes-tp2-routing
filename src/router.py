bf97be373cf3e32b9fdd66823f4bc9bacaeee588
Guilherme Torres <guilhermetorres97@gmail.com>
2018-10-29 05:43:19 -0300

    input OK

--- src/router.py
+++ src/router.py
@@ -1,22 +1,54 @@
 import argparse
 import socket
-import asyncio
+from multiprocessing import Process
 import json
+import time
 
 # tabela de roteamento
 routing_table = []
+distances_table = []
 
-async def send_updates(sckt, local_address, period):
+def add(ip, weight):
+    pass
+
+def remove(ip):
+    pass
+
+def trace(ip):
+    pass
+
+def send_updates(sckt, local_address, period):
     while True:
-        for pair in routing_table:
+        for pair in distances_table:
             dest_address = pair[0]
+            # pega as distancias p cada roteador conhecido
             distances = {}
             message = {'type': 'update', 
             'source': local_address,
             'destination': dest_address,
             'distances': distances
             }
-        await asyncio.sleep(period)
+        time.sleep(period)
+
+def command_line():
+    while True:
+        try:
+            command = input(':>')
+            if command == '':
+                return
+            else:
+                words = command.split(' ')
+                if words[0] == 'add':
+                    add(words[1], words[2])
+                elif words[0] == 'remove':
+                    remove(words[1])
+                elif words[0] == 'trace':
+                    trace(words[1])
+                else:
+                    print('Comando desconhecido.')
+        except EOFError:
+            print('Execucao terminada.')
+            return
 
 def main():
     parser = argparse.ArgumentParser()
@@ -30,6 +62,10 @@ def main():
     sckt.bind((args.addr, 55151))
 
     # faz os updates
-    update_task = asyncio.create_task(send_updates(sckt, args.addr, args.period))
+    update_task = Process(target=send_updates, args=(sckt, args.addr, args.period))
+    update_task.start()
+    command_line()
+    update_task.terminate()
 
-main()
+if __name__ == '__main__':  
+    main()
